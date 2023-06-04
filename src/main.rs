889172deb5e95db7623a0c9a5407a491ec90087b
MuhammadNizamani <ishaque.niz786@gmail.com>
2023-06-04 10:38:05 +0500

    adding post request uisng rust

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,54 @@
-use tide::prelude::*;
+// use tide::prelude::*;
+
+// #[async_std::main]
+// async fn main() -> tide::Result<()> {
+//     async_std::task::spawn(async {
+//         let mut app = tide::new();
+//         app.at("/").get(|_| async { Ok("Hello, World!") });
+//         app.listen("127.0.0.1:8000").await?;
+//         Result::<_, std::io::Error>::Ok(())
+//     })
+//     .await
+//     .unwrap();
+    
+//     Ok(())
+// }
+ // adding post request now 
+use serde::Deserialize;
+use tide::{Request, Response, Body};
+
+#[derive(Deserialize)]
+struct Person {
+    name: String,
+    age: u32,
+}
+
+async fn handle_post(request: Request<Body>) -> tide::Result<Response> {
+    // Deserialize the request body into a Person struct
+    let person: Person = request.body_json().await?;
+    
+    // Access the name and age
+    let name = person.name;
+    let age = person.age;
+    
+    // Process the name and age or perform other operations
+    // ...
+    
+    // Create a response
+    let response = Response::builder(200)
+        .body(format!("Received POST request with name: {} and age: {}", name, age))
+        .build();
+
+    Ok(response)
+}
 
 #[async_std::main]
 async fn main() -> tide::Result<()> {
-    async_std::task::spawn(async {
-        let mut app = tide::new();
-        app.at("/").get(|_| async { Ok("Hello, World!") });
-        app.listen("127.0.0.1:8000").await?;
-        Result::<_, std::io::Error>::Ok(())
-    })
-    .await
-    .unwrap();
+    let mut app = tide::new();
     
+    // Define the POST route
+    app.at("/post").post(handle_post);
+
+    app.listen("127.0.0.1:8000").await?;
     Ok(())
 }
